e60e5761f54657bfb789ee3560f157ecef14441e
annjoeannjoe <76165509+annjoeannjoe@users.noreply.github.com>
2023-06-18 13:52:34 +0800

    Update japanese_lesson1_hiragana_dakuon.php

--- japanese_lesson1_hiragana_dakuon.php
+++ japanese_lesson1_hiragana_dakuon.php
@@ -10,7 +10,7 @@ include 'nav.php';
             margin-bottom: 20px;
             box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
             transition: 0.3s;
-            background-color: #DEEBFF; /* Lighter card background color */
+            background-color: #D9FFF2; /* Lighter card background color */
         }
 
         .card:hover {
@@ -37,6 +37,25 @@ include 'nav.php';
             margin-top: 20px;
             margin-bottom: 20px;
         }
+
+        .container-japan{
+            margin-top: 20px;
+            padding: 30px;
+            background-color: #fdfff2;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+        }
+
+        .back-btn{
+            margin-bottom: 20px;
+            float: left;
+            background-color: #888888;
+            border-color: #888888;
+        }
+
+        .back-btn:hover{
+            background-color: #555555;
+            
+        }
     </style>
 </head>
 <body>
@@ -57,7 +76,7 @@ include 'nav.php';
     <h5><center>Dakuon</center></h5>
     <h6><i><center>Add a symbol to change the sound</center></i></h6>
     <br>
-
+<div class="container-japan">
     <div class="row justify-content-center">
         <?php 
         // Connect to the database
@@ -103,9 +122,11 @@ include 'nav.php';
         <!-- Add an empty column for alignment -->
         <div class="col-md-4"></div>
     </div>
+</div>
 
     <div class="row justify-content-center">
         <div class="col-12 text-right">
+            <a href="japanese_lesson1.php" class="btn btn-primary back-btn">Back</a>
             <a href="japanese_lesson1_hiragana_combo.php" class="btn btn-primary">Next</a>
         </div>
     </div>
